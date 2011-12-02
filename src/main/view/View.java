af0d22e3a2769605bd67f127fa5bbcfcb20b8d9e
danielvarab <djam@itu.dk>
2011-12-02 14:28:00 +0100

    Virker?

--- src/main/view/View.java
+++ src/main/view/View.java
@@ -1,9 +1,18 @@
 package main.view;
 
+import javax.swing.*;
+
 /**
  * The View of the booking-system.
  *
  */
-public class View {
+public class View extends JFrame {
 
+	public View() {
+		pack();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(200, 200);
+		setVisible(true);
+	}
+	
 }
