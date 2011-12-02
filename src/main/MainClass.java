407439964636f4501732ba87991064813bbe9497
Jegp <jensep@gmail.com>
2011-12-02 13:23:40 +0100

    Added main class.

---
+++ src/main/MainClass.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * The main entry point for the booking-system.
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+	
+}
