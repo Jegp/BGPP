e4945ebd4beb5c35cf431c47faa5f45724bc33be
Jegp <jensep@gmail.com>
2011-12-02 22:59:15 +0100

    Merged remote.

--- src/main/view/Container.java
+++ src/main/view/Container.java
@@ -3,8 +3,7 @@ package main.view;
 import java.awt.*;
 import javax.swing.*;
 
-public class Container
-	extends JComponent
-{
-	// Not used yet
+public class Container extends JComponent {
+	// Nice indentation huh?
+	// Trolololo
 }
