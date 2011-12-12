4167f6b8b1c6392250e8d85076a82405d3a14c11
sdebitu <sdeb@itu.dk>
2011-12-12 14:45:34 +0100

    Edited VehicleContainer in view to display a JTable and two JButtons.

--- src/main/view/VehicleContainer.java
+++ src/main/view/VehicleContainer.java
@@ -1,21 +1,48 @@
 package main.view;
 
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
-import javax.swing.JTextField;
-
-public class VehicleContainer
-	extends JPanel
+public class VehicleContainer extends JPanel	
 {
-	
+	JButton addVehicleBtn;
+	JButton deleteBtn;
 	public VehicleContainer()
 	{
-		setLayout(new GridLayout(3, 2));
-		JTextField textField1 = new JTextField(50);
+		//Manage layouts
+		setLayout(new BorderLayout());
+		JPanel center = new JPanel();
+		JPanel south = new JPanel();
+		south.setLayout(new GridLayout());
+		center.setLayout(new GridLayout());
 		
-		add(textField1);
+		
+		//Manage content
+		addVehicleBtn = new JButton("Add Vehicle");
+		deleteBtn = new JButton("Delete");
+		south.add(addVehicleBtn);
+		south.add(deleteBtn);
+		
+			
+		String[] columnNames = {"ID No.", "Manufacter", "Model", "Class"};
+		Object[][] data = {
+				{"10", "Toyota", "Corolla", "2 doors"},{"11", "Suzuki", "Swift", "2 doors"}
+		};		
+		JTable vehicleTable = new JTable(data, columnNames);
+		center.add(new JScrollPane(vehicleTable));
+		add(south, BorderLayout.SOUTH);
+		add(center, BorderLayout.NORTH);
 		
 		setVisible(true);		
+	}
+	
+	public void addVehicleBtnListener(ActionListener vbl) {
+		addVehicleBtn.addActionListener(vbl);
+	}
+	
+	public void addDeleteBtnListener(ActionListener dbl) {
+		deleteBtn.addActionListener(dbl);
 	}
 }
