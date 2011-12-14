adbd555a9c441249decd82ab07740e560c8e68df
daniel.varab <djam@itu.dk>
2011-12-15 00:12:04 +0100

    Contributing to the reservationDisplayer

--- src/main/view/ReservationTable.java
+++ src/main/view/ReservationTable.java
@@ -22,28 +22,34 @@ public class ReservationTable extends AbstractTableModel
 	public ReservationTable(Date startDate, Date endDate) {
 		
 		Period period 				= new Period(startDate, endDate);
-		//Reservation[] reservations 	= Reservation.getFromPeriod(period);
+		Reservation[] reservations 	= Reservation.getFromPeriod(period);
 		periodInDays 				= period.getLengthInDays();
 		Vehicle[] allVehicles 		= Vehicle.getAll();
 		numberOfVehicles 			= allVehicles.length;
 		
 		date 						= new String[periodInDays];
-		data 						= new Object[periodInDays][numberOfVehicles];
+		data 						= new Object[numberOfVehicles][periodInDays];
 		
 		//Initialize calendar
 		GregorianCalendar calendar 	= new GregorianCalendar();
 		calendar.setTime(startDate);
 		
 	 	//Initialize dates
-		for(int i = 0; i < periodInDays; i++) {
-			date[i] = calendar.get(Calendar.DAY_OF_MONTH) + "" + "/" + calendar.get(Calendar.MONTH);	
+		for(int j = 0; j < periodInDays; j++){
+			date[j] = calendar.get(Calendar.DAY_OF_MONTH) + "" + "/" + calendar.get(Calendar.MONTH);
 			calendar.add(Calendar.DAY_OF_MONTH, 1);
-		}
+			}
 		
 		for(int i = 0; i < numberOfVehicles; i++) {
 			for(int j = 0; j < periodInDays; j++) {
-				data[i][j] = "x";
-			}
+				//if(reservations[i].period.isIncluded(calendar.getTime())) {
+				//	data[i][j] = "x";
+				//}
+				//else{
+				//	data[i][j] = "";
+				//}
+				//calendar.add(Calendar.DAY_OF_MONTH, j);
+			}	
 		}
 	}
 
