bdfea713923cff4397d7580ab3ee6f55589e01f6
Jegp <jensep@gmail.com>
2011-12-16 12:41:14 +0100

    Fixed a bug in period in the getLengthInDays method. Added a foxy warning sign when trying to save empty fields in the customer. Added tests to the PeriodTest.

--- src/main/model/Period.java
+++ src/main/model/Period.java
@@ -86,8 +86,9 @@ public class Period extends ModelEntity<Period> {
 	 * Calculates the length between the period in days.
 	 */
 	public int getLengthInDays() {
-																									//  ms     sec  min  hours
-		return (int) (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24);
+		System.out.println(end.getTime() + " " + start.getTime());
+													  //  ms     sec  min  hours = one day
+		return (int) ((end.getTime() - start.getTime()) / (1000L * 60L * 60L * 24L));
 	}
 
 	public String getTable() {
--- src/test/main/model/PeriodTest.java
+++ src/test/main/model/PeriodTest.java
@@ -16,11 +16,12 @@ public class PeriodTest {
 	Period p1;
 	Period p2;
 	Period p3;
+	Period p4;
 
 	// Setup relevant time stuff
-	int minute = 1000 * 60;
-	int hour   = minute * 60;
-	int day    = hour * 24;
+	long minute = 1000 * 60;
+	long hour   = minute * 60;
+	long day    = hour * 24;
 	long now   = System.currentTimeMillis();
 	
 	@Before
@@ -30,10 +31,12 @@ public class PeriodTest {
 		Date d3 = new Date(now - day);
 		Date d4 = new Date(0); // 1970 something something
 		Date d5 = new Date(Long.MAX_VALUE);
+		Date d6 = new Date(day * 1000);
 		
 		p1 = new Period(d1, d2);
 		p2 = new Period(d1, d3);
 		p3 = new Period(d4, d5);
+		p4 = new Period(d4, d6);
 	}
 	
 	@Test
@@ -49,22 +52,27 @@ public class PeriodTest {
 	public void testGetLengthInDays() {
 		int l1 = p1.getLengthInDays();
 		int l2 = p2.getLengthInDays();
+		int l3 = p4.getLengthInDays();
 		
 		assertEquals("Could not calculate length properly.", 1, l1);
 		assertEquals("Could not calculate length properly.", 0, l2);
+		assertEquals("Could not calculate length properly.", 1000, l3);
 	}
 
 	@Test
 	public void testIsIncluded() {
 		Date d1 = new Date(now);
-		Date d2 = new Date(now + 2 * day);
+		Date d2 = new Date(now + day << 2);
 		Date d3 = new Date(now + 1);
 		Date d4 = new Date(Long.MAX_VALUE);
+		Date d5 = new Date(0);
 		
 		assertTrue("Failed to include date", p1.isIncluded(d1));
 		assertFalse("Failed to exclude date", p1.isIncluded(d2));
 		assertFalse("Failed to exclude date", p2.isIncluded(d3));
 		assertTrue("Failed to include date", p3.isIncluded(d3));
+		assertTrue("Failed to include date", p3.isIncluded(d4));
+		assertTrue("Failed to include date", p3.isIncluded(d5));
 	}
 
 }
