9b2a29dcba73b3954631e4b7c624a94307c243da
sdebitu <sdeb@itu.dk>
2011-12-16 12:24:39 +0100

    refactored parts of the vehicletable, in order for vehiclecontroller to get access to crucial variables. Implemented listener to table to create a edit vehicle view

--- src/main/controller/VehicleController.java
+++ src/main/controller/VehicleController.java
@@ -2,6 +2,8 @@ package main.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JOptionPane;
 
@@ -9,18 +11,31 @@ import main.controller.Controller.createActionListenerToCreateButton;
 import main.model.Vehicle;
 import main.model.VehicleClass;
 import main.view.CreateVehicleView;
+import main.view.EditVehicleView;
 import main.view.VehicleContainer;
+import main.view.VehicleTable;
 
 public class VehicleController {
 	
 	private VehicleContainer vehicleContainer;
+	private VehicleTable vehicleTable;
+	private Vehicle[] vehicles;
 	
 	
 	public VehicleController(VehicleContainer vehicleContainer) {
 		
 		this.vehicleContainer = vehicleContainer;
 		
-		vehicleContainer.addCreateVehicleBtnListener(new addVehicleBtnListener());
+		
+		vehicles = Vehicle.getAll();
+		vehicleTable = new VehicleTable(vehicles);
+		
+		vehicleTable.addMouseListener(new EditVehicleListener());
+		vehicleContainer.addTable(vehicleTable);		
+		
+		this.vehicleContainer.addCreateVehicleBtnListener(new addVehicleBtnListener());
+		
+		
 		
 		
 		
@@ -44,12 +59,53 @@ public class VehicleController {
             		VehicleClass vehicleClass = VehicleClass.getWhereId(selectedVehicleClassID);
             		Vehicle v = new Vehicle(description, manufactorer, model, vehicleClass);
         			Vehicle.save(v);
-        			vehicleContainer.updateTable();
+        			vehicleTable.updateTable(Vehicle.getAll());
         			createVehicleView.kill();
         		}
         	}    		
     		});
     	}
+	}
+	
+	class EditVehicleListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			
+			
+			int selectedRow = vehicleTable.getSelectedRow();
+			
+			new EditVehicleView(vehicles[selectedRow]);
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	public void updateTable() {
+		vehicleTable.updateTable(Vehicle.getAll());
 	}
 
 }
--- src/main/view/EditVehicleView.java
+++ src/main/view/EditVehicleView.java
@@ -1,5 +1,70 @@
 package main.view;
 
-public class EditVehicleView {
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import main.model.Vehicle;
+import main.model.VehicleClass;
+
+public class EditVehicleView extends JFrame {
+	
+	private JButton saveButton;
+	private JTextField description;
+	private JTextField manufacter;
+	private JTextField model;
+	private JComboBox vehicleClass;
+	private CancelButton cancelButton;
+	private String[] vehicleClasses;
+	
+	public EditVehicleView(Vehicle vehicle) {
+		
+		super("Edit " + vehicle.manufacturer + " " + vehicle.model);
+		setSize(9000, 9000);
+		setLayout(new GridLayout(5, 2));
+		setResizable(false);
+		fillVehicleClasses();
+		
+		saveButton = new JButton("Save");
+		cancelButton = new CancelButton(this);
+		this.description = new JTextField(vehicle.description);
+		this.manufacter = new JTextField(vehicle.manufacturer);
+		this.model = new JTextField(vehicle.model);
+		this.vehicleClass = new JComboBox(vehicleClasses);
+		
+		//Add content
+		add(new JLabel("Description:"));
+		add(this.description);
+		add(new JLabel("Manufacter:"));
+		add(manufacter);
+		add(new JLabel("Model:"));
+		add(this.model);
+		add(new JLabel("Class:"));
+		add(this.vehicleClass);
+		add(saveButton);
+		add(cancelButton);
+		
+		pack();
+		setVisible(true);
+		
+		
+	}
+	
+	private void fillVehicleClasses() {
+		VehicleClass[] vehicleClassesInDb = VehicleClass.getAll();
+		ArrayList<String> descriptions = new ArrayList<String>();
+		
+		for (int i = 0; i < vehicleClassesInDb.length; i++) {
+			descriptions.add(vehicleClassesInDb[i].description);
+		}
+		
+		vehicleClasses = new String[descriptions.size()];
+		descriptions.toArray(vehicleClasses);
+	}
 
 }
